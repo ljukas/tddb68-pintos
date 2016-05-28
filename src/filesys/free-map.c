2e18e4233a136d7c9b32d442eef7b94094981840
Håkan Gudmundsson <hakgu806@student.liu.se>
2016-05-28 10:34:52 +0200

    Add locks in free-map.c

--- src/filesys/free-map.c
+++ src/filesys/free-map.c
@@ -8,10 +8,14 @@
 static struct file *free_map_file;   /* Free map file. */
 static struct bitmap *free_map;      /* Free map, one bit per disk sector. */
 
+static struct lock free_map_lock;
+
 /* Initializes the free map. */
 void
 free_map_init (void) 
 {
+  lock_init(&free_map_lock);
+
   free_map = bitmap_create (disk_size (filesys_disk));
   if (free_map == NULL)
     PANIC ("bitmap creation failed--disk is too large");
@@ -27,6 +31,8 @@ bool
 free_map_allocate (size_t cnt, disk_sector_t *sectorp) 
 {
   // lås id 6
+  lock_acquire(&free_map_lock);
+
   disk_sector_t sector = bitmap_scan_and_flip (free_map, 0, cnt, false);
   if (sector != BITMAP_ERROR
       && free_map_file != NULL
@@ -37,6 +43,9 @@ free_map_allocate (size_t cnt, disk_sector_t *sectorp)
     }
   if (sector != BITMAP_ERROR)
     *sectorp = sector;
+
+  lock_release(&free_map_lock);
+
   return sector != BITMAP_ERROR;
 }
 
@@ -45,9 +54,13 @@ void
 free_map_release (disk_sector_t sector, size_t cnt)
 {
   // lås id 6
+  lock_acquire(&free_map_lock);
+  
   ASSERT (bitmap_all (free_map, sector, cnt));
   bitmap_set_multiple (free_map, sector, cnt, false);
   bitmap_write (free_map, free_map_file);
+  
+  lock_release(&free_map_lock);
 }
 
 /* Opens the free map file and reads it from disk. */
